32e563cb604fcae3ac8612b22950d6e7f3bded80
asad26 <asad26@users.noreply.github.com>
2016-06-14 20:56:13 +0300

    Update consumer.java

--- kafka-java-codes/consumer.java
+++ kafka-java-codes/consumer.java
@@ -22,35 +22,35 @@ public class Main {
 		Properties props = new Properties();
 		props.put("bootstrap.servers", "192.168.137.155:9092,192.168.137.134:9092,192.168.137.236:9092,192.168.137.63:9092,192.168.137.176:9092");
 		props.put("group.id", "consumerProducer");
-	    props.put("enable.auto.commit", "false");
-	    props.put("auto.commit.interval.ms", "1000");
-	    props.put("auto.offset.reset", "latest");
-	    props.put("session.timeout.ms", "30000");
-	    props.put("heartbeat.interval.ms", "6000");
-	    //props.put("request.timeout.ms", "80000");
-	    //props.put("group.max.session.timeout.ms", "600000");
-	    props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-	    props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-	    String topic1 = "node1";
-	    String topic2 = "node2";
-	    String topic3 = "node3";
-	    String topic4 = "node4";
-	    String topic5 = "node5";
+	    	props.put("enable.auto.commit", "false");
+	    	props.put("auto.commit.interval.ms", "1000");
+	    	props.put("auto.offset.reset", "latest");
+	    	props.put("session.timeout.ms", "30000");
+	    	props.put("heartbeat.interval.ms", "6000");
+	    	//props.put("request.timeout.ms", "80000");
+	    	//props.put("group.max.session.timeout.ms", "600000");
+	    	props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+	    	props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+	    	String topic1 = "node1";
+	    	String topic2 = "node2";
+	    	String topic3 = "node3";
+	    	String topic4 = "node4";
+	    	String topic5 = "node5";
 	    
-	    Properties propsProducer = new Properties();
-	    propsProducer.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "86.50.168.167:9092,86.50.169.10:9092");
-	    propsProducer.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-	    propsProducer.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-	    String topicP = "cameraData";
+	    	Properties propsProducer = new Properties();
+	    	propsProducer.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "86.50.168.167:9092,86.50.169.10:9092");
+	    	propsProducer.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+	    	propsProducer.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+	    	String topicP = "cameraData";
 		 
-	    @SuppressWarnings("resource")
+	    	@SuppressWarnings("resource")
 		KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
 		consumer.subscribe(Arrays.asList(topic1, topic2, topic3, topic4, topic5));
 		
 		@SuppressWarnings("resource")
 		KafkaProducer<String,String> producer = new KafkaProducer<String,String>(propsProducer);
 		
-	    int i = 1;
+	    	int i = 1;
 		while (true) {
 		
 			ConsumerRecords<String, String> records = consumer.poll(10000);
@@ -70,9 +70,9 @@ public class Main {
 					
 					String name = "/data/img" + i + ".jpg";
 					JSONObject jObject = new JSONObject(record.value());
-            		String img_v = jObject.getString("value");
+            				String img_v = jObject.getString("value");
             		
-            		String key = Long.toString(System.currentTimeMillis());
+            				String key = Long.toString(System.currentTimeMillis());
 					ProducerRecord<String, String> data = new ProducerRecord<String, String>(topicP, key, record.value());
 					producer.send(data);
 					
